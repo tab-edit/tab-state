ab210e9421f62ea90baf3b8538c63ea49663474d
Stanley Ihesiulo <stanley.ihesiulo@zymewire.com>
2022-06-26 14:18:55 -0400

    restructured rule categories

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -3,11 +3,11 @@ import LazyLoadingRuleMap from "./utils/lazy-loading-rule-map";
 import deasync from "deasync";
 
 export const builtInRules = new LazyLoadingRuleMap(Object.entries({
-    "explicit-line-name":   () => sync(import("./xml-gen/line-naming/explicit-line-name")),
-    "line-number":          () => sync(import("./xml-gen/line-number")),
-    "note-distance":        () => sync(import("./xml-gen/note-distance")),
-    "measure-count":        () => sync(import("./xml-gen/global-measure-count")),
-    "overhead-repeats":     () => sync(import("./xml-gen/overhead-repeats"))
+    "explicit-line-name":   () => sync(import("./semantics/line-naming/explicit-line-name")),
+    "line-number":          () => sync(import("./semantics/line-number")),
+    "note-distance":        () => sync(import("./semantics/note-distance")),
+    "measure-count":        () => sync(import("./semantics/global-measure-count")),
+    "overhead-repeats":     () => sync(import("./semantics/overhead-repeats"))
 }));
 export default builtInRules;
 
